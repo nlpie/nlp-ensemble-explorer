ed3295244d937ee187e5638a1f63ad4e4c9953a0
Greg Silverman <gms@umn.edu>
2019-12-05 15:39:11 -0600

    major changes for analysis

--- ensemble_explorer/config.py
+++ ensemble_explorer/config.py
@@ -6,12 +6,12 @@ database_type = 'mysql+pymysql' # We use mysql+pymql as default
 database_username = 'gms'
 database_password = 'nej123' 
 database_url = 'localhost' # HINT: use localhost if you're running database on your local machine
-database_name = 'test' # Enter database name
+database_name = 'concepts' # Enter database name
 
 engine_request = str(database_type)+'://'+database_username+':'+database_password+"@"+database_url+'/'+database_name
 engine = create_engine(engine_request, pool_pre_ping=True, pool_size=20, max_overflow=30)
 
-data_dir =  '/Users/gms/development/nlp/nlpie/data/amicus-u01/output/'
+data_dir =  '/Users/gms/development/nlp/nlpie/data/ensembling-u01/output/'
 
 single_sys_dir = Path(data_dir + "single_system_out")
 single_sys_dir.mkdir(parents=True, exist_ok=True)
@@ -19,8 +19,10 @@ dir_out = Path(data_dir + 'single_system_out/')
 
 systems = ['biomedicus', 'clamp', 'metamap'] 
 
-corpus = 'casi' 
+corpus = 'fairview'
+analysis_type = 'entity'
 
-system_annotations = 'analytical_cui_' + corpus + '_concepts.csv'
+#system_annotations = 'analytical_cui_' + corpus + '_concepts.csv'
+system_annotations = 'analytical_'+corpus+'.csv'
 
 reference_annotations = corpus + '_all'
