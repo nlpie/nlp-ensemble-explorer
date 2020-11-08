82e48435fda665abfd02c63866b9b18ae654f42a
Greg Silverman <gms@umn.edu>
2020-11-08 11:03:25 -0600

    more merge

---
+++ ensemble_explorer/import_from_spacy.py
@@ -0,0 +1,62 @@
+import os
+import spacy
+import pandas as pd
+import time
+from scispacy.umls_linking import UmlsEntityLinker
+linker = UmlsEntityLinker(resolve_abbreviations=True)
+from sqlalchemy.types import Integer, String, Float
+from config import engine, input_folder
+
+
+
+database_type = 'mysql+pymysql' # We use mysql+pymql as default
+corpus = 'mipacq'
+files = os.listdir(input_folder)
+for model in ("en_core_sci_sm","en_core_sci_md","en_core_sci_lg",
+              "en_ner_craft_md", "en_ner_jnlpba_md","en_ner_bc5cdr_md",
+              "en_ner_bionlp13cg_md",):
+    nlp = spacy.load(model)
+    nlp.add_pipe(linker)
+    os.chdir(input_folder)
+    start_time = time.time()
+    for i, file in enumerate(files):
+        with open(file) as f:
+            print(file)
+            doc = nlp(f.read().replace('^', ' '))
+            df = pd.DataFrame(
+                [
+                    [ent.end_char, ent.start_char, ent.text,
+                    int(ent._.umls_ents[0][0][1:]), ent._.umls_ents[0][1],
+                    linker.umls.semantic_type_tree.type_id_to_node[
+                        linker.umls.cui_to_entity[ent._.umls_ents[0][0]].types[0]
+                    ].type_id]
+                    if len(ent._.umls_ents) > 0
+                    else [ent.end_char, ent.start_char, ent.text,None, None, None]
+                    for ent in doc.ents
+                ],
+                columns=['end', 'begin', 'string', 'cui', 'score', 'semantic_type']
+            )
+            df['file'] = file[:-4]
+            df['corpus'] = 'mipacq'
+            df['system'] = model
+            df['type'] = None
+            df.to_sql('spacy', if_exists='append', con=engine, index=False,
+                dtype={
+                'cui': Integer(),
+                'begin': Integer(),
+                'end': Integer(),
+                'corpus': String(),
+                'system': String(),
+                'type': String(),
+                'score': Float(),
+                'semantic_type': String(),
+                'string': String(),
+                })
+
+    time_per_file = (time.time() - start_time) / i
+    print(f'time per file: {time_per_file}')
+
+
+
+    #grouping by end, start, and file converts these columns into a multiindex,
+    #which makes splitting by filename faster
---
+++ ensemble_explorer/import_mipacq_ann_4_ben.py
@@ -0,0 +1,31 @@
+from config import output_folder, engine
+from sqlalchemy.types import Integer, String, Float
+import pandas as pd
+import os
+os.chdir(output_folder)
+df = pd.read_csv('mipacq_ann_4_ben.csv')
+def convert_cui(x):
+    try:
+        return int(x[1:])
+    except ValueError:
+        return None
+df['cui'] = df['value'].apply(convert_cui)
+df['begin'] = df['start']
+df['corpus'] = 'mipacq'
+df['system'] = 'reference'
+df['type'] = None
+df['string'] = df['text']
+df['semantic_type'] = df['class']
+df = df.drop(['text', 'mentionSlot', 'start', 'class', 'value', 'classtype'], axis=1)
+df.to_sql('spacy', if_exists='append', con=engine, index=False,
+    dtype={
+    'cui': Integer(),
+    'begin': Integer(),
+    'end': Integer(),
+    'corpus': String(),
+    'system': String(),
+    'type': String(),
+    'score': Float(),
+    'semantic_type': String(),
+    'string': String(),
+    })
